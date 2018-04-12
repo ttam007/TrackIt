d415e597d718fa1e48572d4e429b11af0ab1d423
StvnLunsford <StvnLunsford@gmail.com>
2018-04-12 18:05:59 -0400

    First run at Check-in/out GUI

--- TrackIt/src/trackit/UI/CheckInOutUI.java
+++ TrackIt/src/trackit/UI/CheckInOutUI.java
@@ -14,17 +14,23 @@ public class CheckInOutUI
     // <editor-fold defaultstate="collapsed" desc="Constants">
 
     private static final String WINDOW_NAME = "Check In/Out";
-    // </editor-fold>
+// </editor-fold>
     // <editor-fold defaultstate="expanded" desc="Private Fields">
+    private final InventoryItem testItem = new InventoryItem();
     // </editor-fold>
     // <editor-fold defaultstate="collapsed" desc="Components">
-    JPanel pnlMain = new JPanel();
-    JButton btnOK = new JButton();
-    JButton btnCancel = new JButton();
+    JPanel pnlMain;
+    JButton btnOK, btnCancel;
+    JRadioButton inButton, outButton;
+    JComboBox<String> itemComboBox;
+    JLabel itemNameLabel, qtyLabel;
+    JTextField qtyTextField;
+    String[] itemStrings = { "soap", "shampoo", "conditioner", "paper towels", "mouthwash" };
 
     // </editor-fold>
     // <editor-fold defaultstate="collapsed" desc="Constructors">
     public CheckInOutUI() {
+    
         initializeComponents();
     }
     // </editor-fold>
@@ -35,26 +41,69 @@ public class CheckInOutUI
      */
     private void initializeComponents() {
         //Setup main frame
-        int frameWidth = 1200;
-        int frameHeight = 600;
+        int frameWidth = 500;
+        int frameHeight = 250;
         Dimension dimFrame = new Dimension(frameWidth, frameHeight);
         this.setTitle(Utilities.getWindowCaption(WINDOW_NAME));
         this.setPreferredSize(dimFrame);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
-        this.setDefaultCloseOperation(0);
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.addWindowListener(new CloseQuery());
+        this.setVisible(true);
 
         //Add all components here and set properties.
-        this.add(pnlMain);
-        this.add(this.btnOK);
+        Box buttonBx, itemBx, qtyBx, submitBx, combine;
+        
+        pnlMain = new JPanel();
+        add(pnlMain, BorderLayout.CENTER);
+        
+        //create the Radio Buttons and add them to a group
+        buttonBx = Box.createHorizontalBox();
+        ButtonGroup checkGroup = new ButtonGroup();
+        inButton = new JRadioButton("Check In");
+        outButton = new JRadioButton("Check Out");
+        checkGroup.add(inButton);
+        checkGroup.add(outButton);
+        inButton.setSelected(true);
+        buttonBx.add(inButton);
+        buttonBx.add(outButton);
+        
+        //item selection
+        itemBx = Box.createHorizontalBox();
+        itemNameLabel = new JLabel("Item Name");
+        itemBx.add(itemNameLabel);
+        itemComboBox = new JComboBox(itemStrings);
+        itemBx.add(itemComboBox);
+        qtyBx = Box.createHorizontalBox();
+        qtyLabel = new JLabel("Quantity");
+        qtyBx.add(qtyLabel);
+        qtyTextField = new JTextField();
+        qtyBx.add(qtyTextField);
+        submitBx = Box.createHorizontalBox();
+        btnOK = new JButton("OK");
+        
         this.btnOK.addActionListener((ActionEvent e) -> {
-            //TODO
+            if(!testItem.save()) {
+                
+            }
         });
-        this.add(this.btnCancel);
+        
+        btnCancel = new JButton("Cancel");
+        submitBx.add(btnCancel);
+        
         this.btnCancel.addActionListener((ActionEvent e) -> {
-            //TODO
+            //TODO:  close window and return to prior window.
         });
-
+        
+        //add all of the boxes together
+        combine = Box.createVerticalBox();
+        combine.add(buttonBx);
+        combine.add(itemBx);
+        combine.add(qtyBx);
+        combine.add(submitBx);
+        
+        pnlMain.add(combine);
         //Finalizations
         pack();
     }
@@ -68,6 +117,32 @@ public class CheckInOutUI
         System.out.println(String.format("Displaying {0}...", WINDOW_NAME));
         setVisible(true);
     }
+    
+    public void closeWindow() {
+        this.setVisible(false);
+    }
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="SubClasses">
+    /**
+     * Handles all aspects of closing the program.
+     */
+    private class CloseQuery extends WindowAdapter {
 
+        @Override
+        public void windowClosing(WindowEvent e) {
+            JFrame frame = (JFrame) e.getSource();
+            int result = JOptionPane.showConfirmDialog(frame,
+                    "Do you want to save?", "Close Query",
+                    JOptionPane.YES_NO_OPTION);
+            if (result == JOptionPane.YES_OPTION) {
+                //TODO
+                JOptionPane.showMessageDialog(null, "Successfully Updated");
+                closeWindow();
+            } else {
+                //TODO
+                closeWindow();
+            }
+        }
+    }
     // </editor-fold>
 }
