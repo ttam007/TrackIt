7ad0dc3930af5a1dfcddd3989dd901ab8e7a16f3
bme1979 <37425143+bme1979@users.noreply.github.com>
2018-04-11 21:10:04 -0400

    Add files via upload

---
+++ TrackIt/src/trackit/InventoryItemsUI.java
@@ -0,0 +1,66 @@
+package trackit.UI;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import trackit.*;
+
+/**
+ * UI Layer: Handles all aspects of the Inventory panel. TODO: convert to
+ * JPanel.
+ */
+public class InventoryItemsUI
+        extends JPanel {
+    // <editor-fold defaultstate="collapsed" desc="Constants">
+
+    private static final String WINDOW_NAME = "Inventory";
+    // </editor-fold>
+    // <editor-fold defaultstate="expanded" desc="Private Fields">
+    private final ArrayList<InventoryItem> inventoryItems = new ArrayList<>();
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Components">
+    JButton btnAddToList = new JButton();
+    JButton btnRemoveFromList = new JButton();
+
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Constructors">
+    public InventoryItemsUI() {
+        
+
+        refreshItems();
+    }
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Private Methods">
+
+  
+    /**
+     * Refreshes the list of items that are displayed in the grid.
+     */
+    private void refreshItems() {
+        this.inventoryItems.clear();
+
+        //TODO:  load items from database.
+    }
+
+    /**
+     * Launches the Item Detail window.
+     *
+     * @param anItem The item to be shown.
+     */
+    private void showItemDetails(Item anItem) {
+        //bll.showDialog(anItem);    
+    }
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Public Methods">
+
+    /**
+     * Displays the frame.
+     */
+    public void display() {
+        System.out.println(String.format("Displaying {0}...", WINDOW_NAME));
+        setVisible(true);
+    }
+
+    // </editor-fold>
+}
