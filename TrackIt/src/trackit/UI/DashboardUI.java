6603bcaec6f7187f4967408ba9683f51a2e45572
btdiaz34 <brian.diaz@gfm247.com>
2018-04-13 19:09:10 -0400

    Added a text area to the panel with dummy data to simulate similar notifications in production

--- TrackIt/src/trackit/UI/DashboardUI.java
+++ TrackIt/src/trackit/UI/DashboardUI.java
@@ -10,17 +10,20 @@ import trackit.*;
  * UI Layer: Handles all aspects of the Dashboard panel.
  */
 public class DashboardUI extends JPanel {
+    private JTextArea dashboardInfo;
+    private JFrame mainFrame;
 
     /**
      * Creates new form DashboardUI
      */
     public DashboardUI() {
         initializeComponents();
+        this.setVisible(true);
     }
 
     // <editor-fold defaultstate="collapsed" desc="Private Methods">
     private void initializeComponents() {
-
+        /*
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -30,7 +33,17 @@ public class DashboardUI extends JPanel {
         layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGap(0, 300, Short.MAX_VALUE)
-        );
+        );*/
+
+        StringBuilder sb = new StringBuilder("- 5 items are out of stock\n- Milk will expire in 3 days\n- Order arriving today\n");
+
+        dashboardInfo = new JTextArea(35,90);
+        dashboardInfo.setText(sb.toString());
+        dashboardInfo.setEditable(false);
+
+        JScrollPane sp = new JScrollPane(dashboardInfo);
+        sp.setSize(new Dimension(1000,400));
+        add(sp);
     }
     // </editor-fold>
 }
