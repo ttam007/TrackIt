a7059bbbadd044112f16cc34ce4b7afdd914c3f3
btdiaz34 <brian.diaz@gfm247.com>
2018-04-13 20:05:20 -0400

    Fixed runttime errors

--- TrackIt/src/trackit/UI/DashboardUI.java
+++ TrackIt/src/trackit/UI/DashboardUI.java
@@ -1,6 +1,6 @@
 package trackit.UI;
 
-import java.awt.*;
+import java.awt.Dimension;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
@@ -23,17 +23,6 @@ public class DashboardUI extends JPanel {
 
     // <editor-fold defaultstate="collapsed" desc="Private Methods">
     private void initializeComponents() {
-        /*
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 300, Short.MAX_VALUE)
-        );*/
 
         StringBuilder sb = new StringBuilder("- 5 items are out of stock\n- Milk will expire in 3 days\n- Order arriving today\n");
 
