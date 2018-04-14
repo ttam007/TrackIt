6acd64f59eb59576cc8b64ed46fc90bbaf002dc4
StvnLunsford <StvnLunsford@gmail.com>
2018-04-14 19:25:57 -0400

    Creating test driver methods for SQLHelper classes

--- TrackItTestDriver/src/trackittestdriver/DALTestDriver.java
+++ TrackItTestDriver/src/trackittestdriver/DALTestDriver.java
@@ -117,7 +117,32 @@ public class DALTestDriver {
                     aSupplier.getPrimaryKey(), aSupplier.getNickname(), aSupplier.getUrl()));
         }
     }
+    
+    private void printOrderItem(AnOrderItem anOrderItem) {
+        if (anOrderItem == null) {
+            System.out.println("OrderItem is null.");
+        } else {
+            System.out.println(String.format("OrderItem:  PK = %d; Order ID = %d;"
+                    + " Quantity Ordered = %d; quantity checked in = %d; Price = %f;"
+                    + " Extended Price = %f",
+                    anOrderItem.getPrimaryKey(), anOrderItem.getOrderId(), 
+                    anOrderItem.getQuantityOrdered(), anOrderItem.getQuantityCheckedIn(), 
+                    anOrderItem.getPrice(), anOrderItem.getExtendedPrice()));
+        }
+    }
 
+    private void printOrder(AnOrder anOrder) {
+        if (anOrder == null) {
+            System.out.println("Order is null.");
+        } else {
+            System.out.println(String.format("Order:  PK = %d; Description = %s; "
+                    + "Ordered From = %s; Order Status = %s; Date Ordered = %s; "
+                    + "Extended Price = %s", 
+                    anOrder.getPrimaryKey(), anOrder.getDescription(), 
+                    anOrder.getOrderedFrom(), anOrder.getOrderStatus(), 
+                    anOrder.getDateOrdered(), anOrder.getDateExpected()));
+        }
+    }
     /**
      * Tests all CRUD operations for the ASupplier's sprocs.
      */
@@ -161,6 +186,109 @@ public class DALTestDriver {
             helper.delete(pk);
             aSupplier = helper.selectOne(pk);
             printSupplier(aSupplier);
+        } catch (SQLException exSQL) {
+            System.out.println("SQL error = " + exSQL.getLocalizedMessage());
+        } catch (Exception ex) {
+            System.out.println("Generic error = " + ex.getLocalizedMessage());
+        }
+    }
+    
+    public void testSQLHelperOrderItem() {
+        System.out.println("\n\ntestSQLHelperOrderItem");
+
+        SQLHelperOrderItem helper = new SQLHelperOrderItem();
+        AnOrderItem anOrderItem;
+        Integer pk;
+        try {
+            System.out.println("\nSelectAll");
+            ArrayList<AnOrderItem> orderItems = helper.selectAll();
+            for (AnOrderItem anItem : orderItems) {
+                printOrderItem(anItem);
+            }
+
+            System.out.println("\nSelectOne");
+            anOrderItem = helper.selectOne(2);
+            printOrderItem(anOrderItem);
+
+            System.out.println("\nInsert");
+            //Must delete "Etsy" from the database before testing this.
+            AnOrderItem anOrderItemInsert = new AnOrderItem();
+            anOrderItemInsert.setOrderId(46532);
+            anOrderItemInsert.setQuantityOrdered(3);
+            anOrderItemInsert.setQuantityCheckedIn(2);
+            anOrderItemInsert.setPrice(2.39);
+            pk = helper.insert(anOrderItemInsert);
+            anOrderItem = helper.selectOne(pk);
+            printOrderItem(anOrderItem);
+
+            System.out.println("\nUpdate");
+            AnOrderItem anOrderItemUpdate = new AnOrderItem();
+            anOrderItemUpdate.setPrimaryKey(pk);
+            anOrderItemUpdate.setOrderId(46532);
+            anOrderItemUpdate.setQuantityCheckedIn(3);
+            anOrderItemUpdate.setPrice(2.49);
+            helper.update(anOrderItemUpdate);
+            anOrderItem = helper.selectOne(anOrderItemUpdate.getPrimaryKey());
+            printOrderItem(anOrderItem);
+
+            System.out.println("\nDelete");
+            pk = 2;
+            helper.delete(pk);
+            anOrderItem = helper.selectOne(pk);
+            printOrderItem(anOrderItem);
+        } catch (SQLException exSQL) {
+            System.out.println("SQL error = " + exSQL.getLocalizedMessage());
+        } catch (Exception ex) {
+            System.out.println("Generic error = " + ex.getLocalizedMessage());
+        }
+    }
+    
+    public void testSQLHelperOrder() {
+        System.out.println("\n\ntestSQLHelperOrder");
+
+        SQLHelperOrder helper = new SQLHelperOrder();
+        AnOrder anOrder;
+        Integer pk;
+        try {
+            System.out.println("\nSelectAll");
+            ArrayList<AnOrder> order = helper.selectAll();
+            for (AnOrder anItem : order) {
+                printOrder(anItem);
+            }
+
+            System.out.println("\nSelectOne");
+            anOrder = helper.selectOne(2);
+            printOrder(anOrder);
+
+            System.out.println("\nInsert");
+            //Must delete "Etsy" from the database before testing this.
+            AnOrder anOrderInsert = new AnOrder();
+            anOrderInsert.setDescription("Cleaning Supplies");
+            anOrderInsert.setOrderedFrom(2);
+            anOrderInsert.setOrderStatus("In Progress");
+            anOrderInsert.setDateOrdered();
+            anOrderInsert.setDateExpected()
+            pk = helper.insert(anOrderInsert);
+            anOrder = helper.selectOne(pk);
+            printOrder(anOrder);
+
+            System.out.println("\nUpdate");
+            AnOrder anOrderUpdate = new AnOrder();
+            anOrderUpdate.setPrimaryKey(pk);
+            anOrderUpdate.setDescription("Cleaning Supplies");
+            anOrderUpdate.setOrderedFrom("1");
+            anOrderUpdate.setOrderStatus("Shipped");
+            anOrderUpdate.setDateOrdered(dateOrdered);
+            anOrderUpdate.setDateExpected(dateExpected);
+            helper.update(anOrderUpdate);
+            anOrder = helper.selectOne(anOrderUpdate.getPrimaryKey());
+            printOrder(anOrder);
+
+            System.out.println("\nDelete");
+            pk = 2;
+            helper.delete(pk);
+            anOrder = helper.selectOne(pk);
+            printOrder(anOrder);
         } catch (SQLException exSQL) {
             System.out.println("SQL error = " + exSQL.getLocalizedMessage());
         } catch (Exception ex) {
