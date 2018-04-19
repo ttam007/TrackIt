941a4397cb1204bf6cc1b785b399a2e9da2bd026
btdiaz34 <brian.diaz@gfm247.com>
2018-04-19 10:14:12 -0400

    Cleaned up comments and other bs.

--- TrackIt/src/trackit/InventoryItemsTableModel.java
+++ TrackIt/src/trackit/InventoryItemsTableModel.java
@@ -2,46 +2,20 @@ package trackit;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import javax.swing.table.AbstractTableModel;
-
 import trackit.DAL.SQLHelperInventoryItem;
-import trackit.UI.InventoryItemsPanel;
+
 
 
 /**
  * @author  Diaz
- * @purpose Handles the dynamic creation of the JTable
+ * @purpose Handles the dynamic creation of the JTable by getting data from DB
  */
-public class InventoryItemsTableModel extends AbstractTableModel{
+public class InventoryItemsTableModel {
     SQLHelperInventoryItem helper = new SQLHelperInventoryItem();
-    private final String[] columnNames = InventoryItemsPanel.getColumnNames();
     ArrayList<AnInventoryItem> inventoryItems = getSQL();
-    Object[] allInventoryItems =  getSQL().toArray();
 
-
-
-    @Override
-    public int getRowCount() {
-        return allInventoryItems.length;
-    }
-
-    @Override
-    public int getColumnCount() {
-        return columnNames.length;
-    }
-
-    @Override
-    public String getColumnName(int col) {
-        return columnNames[col];
-    }
-
-    @Override
-    public Object getValueAt(int i, int i1) {
-        return allInventoryItems[i];
-    }
     ArrayList<AnInventoryItem> getSQL() {
         try {
-            System.out.println("\nSelectAll");
             inventoryItems = helper.selectAll();
         } catch (SQLException exSQL) {
             System.out.println("SQL error = " + exSQL.getLocalizedMessage());
